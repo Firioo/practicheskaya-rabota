84705b9e1f08f8ccf66075aac1e61d0f461520bf
Firioo <meloamnlikemusic@gmail.com>
2024-12-18 07:32:01 +0300

    Проверка на палиндром

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите строку для проверки на палиндром:");
+        string input = Console.ReadLine();
+
+        if (Check.IsPalindrome(input))
+        {
+            Console.WriteLine("Строка является палиндромом.");
+        }
+        else
+        {
+            Console.WriteLine("Строка не является палиндромом.");
+        }
+    }
+
+
+}
---
+++ ConsoleApp1/check.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Check
+{
+    public static bool IsPalindrome(string str)
+    {
+        // Убираем пробелы и приводим строку к одному регистру
+        string cleanedStr = str.Replace(" ", "").ToLower();
+
+        int left = 0;
+        int right = cleanedStr.Length - 1;
+
+        while (left < right)
+        {
+            if (cleanedStr[left] != cleanedStr[right])
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
